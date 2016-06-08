de2ea1fffca0c93211ace683c0f6a983f4ca9398
Yong Whoan Kim <Andy@dhcp-v232-187.pv.reshsg.uci.edu>
2016-06-07 17:08:50 -0700

    Initial Commit

---
+++ money4kids/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  money4kids
+//
+//  Created by Yong Whoan Kim on 5/21/16.
+//  Copyright © 2016 Yong Whoan Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ money4kidsTests/money4kidsTests.swift
@@ -0,0 +1,36 @@
+//
+//  money4kidsTests.swift
+//  money4kidsTests
+//
+//  Created by Yong Whoan Kim on 5/21/16.
+//  Copyright © 2016 Yong Whoan Kim. All rights reserved.
+//
+
+import XCTest
+@testable import money4kids
+
+class money4kidsTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
